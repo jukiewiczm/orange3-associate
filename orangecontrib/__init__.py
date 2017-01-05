ab4e17448daedccc59bd3fd68abd883a7cd1a826
mjukiewicz1 <jukiewiczm@gmail.com>
2017-01-05 15:37:04 +0100

    Additional future import

--- orangecontrib/__init__.py
+++ orangecontrib/__init__.py
@@ -1,5 +1,6 @@
 from __future__ import division
 from __future__ import print_function
+from __future__ import unicode_literals
 __import__("pkg_resources").declare_namespace(__name__)
 # orangecontrib is a namespace modules shared by multiple Orange add-on so it
 # needs to declare namespace.
